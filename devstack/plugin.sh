c610928f6f675e2fec89c9592b57534a26a1d693
Kristi Nikolla <knikolla@bu.edu>
2016-05-13 13:08:32 -0400

    Added pki_setup

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -8,6 +8,8 @@ function install_idp(){
 
 function configure_idp(){
     sudo python $K2K_IDP_SCRIPTS/configure_keystone.py $IDP_IP
+
+    keystone-manage pki_setup
     keystone-manage saml_idp_metadata > /etc/keystone/keystone_idp_metadata.xml
 
     # Restart Apache2/httpd
